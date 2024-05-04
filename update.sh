ce40946972cfa4c0416ebf580e0da8bfca9e966c
elprofesor <rrazvan.radeanu@gmail.com>
2024-05-05 01:09:19 +0300

    new update.sh methodology

--- update.sh
+++ update.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+rm -rf Packages Packages.gz Release InRelease Release.gpg
+
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 
 echo "Origin: ppa.elprofesor.io" > Release
 echo "Label: ppa.elprofesor.io" >> Release
 echo "Suite: stable" >> Release
-echo "Version: 1337" >> Release
+echo "Version: 1" >> Release
 echo "SHA256: $(sha256sum Release | awk '{print $1}')" >> Release
 echo "Date: $(date -R)" >> Release
-echo "Codename: elprofesor" >> Release
+echo "Codename: stable" >> Release
 echo "Architectures: all" >> Release
 echo "Components: main" >> Release
 echo "Description: ppa.elprofesor.io" >> Release
 
-rm -rf Release.gpg; gpg  -abs -o - Release > Release.gpg
-rm -rf inRelease; gpg --clearsign -o - Release > InRelease
+gpg  -abs -o - Release > Release.gpg
+gpg --clearsign -o - Release > InRelease
 

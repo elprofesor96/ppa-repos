1975d81d4f177b6701dd11989d3a25dd2b0b6436
elprofesor <rrazvan.radeanu@gmail.com>
2024-05-05 01:58:00 +0300

    add sha1, md5 in release

--- update.sh
+++ update.sh
@@ -10,6 +10,8 @@ echo "Label: ppa.elprofesor.io" >> Release
 echo "Suite: stable" >> Release
 echo "Version: 1" >> Release
 echo "SHA256: $(sha256sum Release | awk '{print $1}')" >> Release
+echo "SHA1: $(sha1sum Packages | awk '{print $1}')" >> Release 
+echo "MD5: $(md5sum Packages | awk '{print $1}')" >> Release
 echo "Date: $(date -R)" >> Release
 echo "Codename: stable" >> Release
 echo "Architectures: all" >> Release

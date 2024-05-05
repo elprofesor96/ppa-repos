ea298c81fc98df9e832bcbe6f9c87cbc2590bd03
elprofesor <rrazvan.radeanu@gmail.com>
2024-05-05 03:41:03 +0300

    add all hashes in Release

--- update.sh
+++ update.sh
@@ -9,15 +9,32 @@ echo "Origin: ppa.elprofesor.io" > Release
 echo "Label: ppa.elprofesor.io" >> Release
 echo "Suite: stable" >> Release
 echo "Version: 1.0" >> Release
-echo "MD5Sum: $(md5sum Packages)" >> Release
-echo "SHA1: $(sha1sum Packages)" >> Release 
-echo "SHA256: $(sha256sum Release)" >> Release
-echo "SHA512: $(sha512sum Packages)" >> Release
 echo "Date: $(date -R)" >> Release
 echo "Codename: stable" >> Release
 echo "Architectures: all" >> Release
 echo "Components: main" >> Release
 echo "Description: ppa.elprofesor.io" >> Release
+
+echo "MD5Sum:" >> Release
+echo " $(md5sum Packages | awk '{print $1}') $(wc -c < Packages) Packages" >> Release
+echo " $(md5sum Packages.gz | awk '{print $1}') $(wc -c < Packages.gz) Packages.gz" >> Release
+echo " $(md5sum Release | awk '{print $1}') $(wc -c < Release) Release" >> Release
+
+echo "SHA1:" >> Release
+echo " $(sha1sum Packages | awk '{print $1}') $(wc -c < Packages) Packages" >> Release
+echo " $(sha1sum Packages.gz | awk '{print $1}') $(wc -c < Packages.gz) Packages.gz" >> Release
+echo " $(sha1sum Release | awk '{print $1}') $(wc -c < Release) Release" >> Release
+
+echo "SHA256:" >> Release
+echo " $(sha256sum Packages | awk '{print $1}') $(wc -c < Packages) Packages" >> Release
+echo " $(sha256sum Packages.gz | awk '{print $1}') $(wc -c < Packages.gz) Packages.gz" >> Release
+echo " $(sha256sum Release | awk '{print $1}') $(wc -c < Release) Release" >> Release
+
+echo "SHA512:" >> Release
+echo " $(sha512sum Packages | awk '{print $1}') $(wc -c < Packages) Packages" >> Release
+echo " $(sha512sum Packages.gz | awk '{print $1}') $(wc -c < Packages.gz) Packages.gz" >> Release
+echo " $(sha512sum Release | awk '{print $1}') $(wc -c < Release) Release" >> Release
+
 
 gpg  -abs -o - Release > Release.gpg
 gpg --clearsign -o - Release > InRelease
